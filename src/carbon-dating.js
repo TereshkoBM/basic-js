1c192e105b3fee5b2fa09f6bd33b178c5b4f3e69
Boris Tereshko <tbm_mail@tut.by>
2022-05-01 10:56:32 +0300

    fix: carboon-dating,js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,11 +17,22 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  
+  if (typeof sampleActivity !== 'string') return false;
+  if (sampleActivity === '') return false;
+  let num = Number(sampleActivity);
+  if (isNaN(num)) return false;
+  if (num <= 0 || num > 15) return false;
+  
+  return Math.ceil(Math.log(MODERN_ACTIVITY / num) * HALF_LIFE_PERIOD / 0.693);
+
 }
 
 module.exports = {
   dateSample
 };
+
+
